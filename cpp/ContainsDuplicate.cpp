85650c2f506f070be8f6808092447195867afbf8
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-15 23:34:00 +0100

    Contains duplicate: code refactoring

--- cpp/ContainsDuplicate.cpp
+++ cpp/ContainsDuplicate.cpp
@@ -7,19 +7,13 @@ class Solution
 public:
     bool containsDuplicate(vector<int> &nums)
     {
-        // adding a map
         unordered_map<int, int> map;
-        for (int i = 0; i < nums.size() - 1; i++)
+        for (int num : nums)
         {
             // compare each element with the existing elements in the map if it is already there return 1
-            if (map.find(nums[i]) == map.end())
-            {
-                map[nums[i]] = 1;
-            }
-            else
-            {
+            if (map.find(num) != map.end())
                 return 1;
-            }
+            map[num] = 1;
         }
         return 0;
     }
