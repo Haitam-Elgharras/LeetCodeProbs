d6e2652375b3817dcddc7d7760d7eb8aa793f853
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-15 23:22:35 +0100

    Performance optimization: search with unordered map O(1)

--- cpp/ContainsDuplicate.cpp
+++ cpp/ContainsDuplicate.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 class Solution
@@ -6,12 +7,18 @@ class Solution
 public:
     bool containsDuplicate(vector<int> &nums)
     {
+        // adding a map
+        unordered_map<int, int> map;
         for (int i = 0; i < nums.size() - 1; i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            // compare each element with the existing elements in the map if it is already there return 1
+            if (map.find(nums[i]) == map.end())
             {
-                if (nums[i] == nums[j])
-                    return 1;
+                map[nums[i]] = 1;
+            }
+            else
+            {
+                return 1;
             }
         }
         return 0;
