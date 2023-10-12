5ec73b49923252843a20ad1635742d239204f397
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-10-12 10:16:49 +0100

    Counter problem: Closures

---
+++ js/counter.js
@@ -0,0 +1,20 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+var counter = createCounter(5);
+console.log(counter()); // 5
+console.log(counter()); // 6
+console.log(counter()); // 7
+
+/* EXPLICATION:
+
+The mechanism by which the inner function (the one returned by createCounter) "knows" the value 
+of n is through a concept in JavaScript called closures. Closures allow inner functions to access
+ variables from their outer (enclosing) function even after the outer function has finished executing.
+ 
+ */
