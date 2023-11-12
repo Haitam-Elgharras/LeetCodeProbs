758593de613ef5a4b18d3c364b444a86d90edd48
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-11-12 23:57:30 +0100

    AddingTwoIntegers without + optimal solution beats 100% c++ Users

--- cpp/AddTwoIntegers.cpp
+++ cpp/AddTwoIntegers.cpp
@@ -4,36 +4,61 @@
 
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     int getSum(int a, int b)
+//     {
+//         float num = a - b;
+
+//         switch ((int)num)
+//         {
+//         case 1:
+//             return a * 2 - 1;
+//             break;
+//         case -1:
+//             return b * 2 - 1;
+//             break;
+//         default:
+//             break;
+//         }
+
+//         float current = 0;
+//         if (num > 0)
+//         {
+//             num = num / 2;
+//             current = a - num;
+//             return (int)(current * 2);
+//         }
+
+//         num = -num / 2;
+//         current = b - num;
+
+//         return (int)(current * 2);
+//     }
+// };
+
 class Solution
 {
 public:
     int getSum(int a, int b)
     {
-        float num = a - b;
-        if (num == 1)
-        {
-            return a * 2 - 1;
-        }
-        if (num == -1)
-        {
-            return b * 2 - 1;
-        }
-        float current = 0;
-        if (num > 0)
-        {
-            num = num / 2;
-            current = a - num;
+        int result = 0;
 
-            return (int)(current * 2);
-        }
-        else
-        {
-            num = -num / 2;
-            cout << num << endl;
-            current = b - num;
+        int difference = (a > b) ? (a - b) : (b - a);
 
-            return (int)(current * 2);
+        switch (difference)
+        {
+        case 1:
+            result = (a > b) ? (a * 2 - 1) : (b * 2 - 1);
+            break;
+        default:
+            float num = (float)difference / 2;
+            result = (a > b) ? (int)((a - num) * 2) : (int)((b - num) * 2);
+            break;
         }
+
+        return result;
     }
 };
 
